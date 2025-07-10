90ce80a05c0eb9cd640a08b5e085758b1db2fe74
michil <fitim1501@gmail.com>
2025-07-10 09:52:57 +0900

    done with domain models

---
+++ DevQuestions/src/DevQuestions.Domain/Report/Report.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevQuestions.Domain.Report
+{
+    public class Report
+    {
+        public Guid Id { get; set; }
+        public required Guid UserId { get; set; }
+        public required Guid ReportedUserId { get; set; }
+        public Guid? ResolvedByUserId { get; set; }
+        public required string Reason { get; set; }
+        public Status Status { get; set; } = Status.Open;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
