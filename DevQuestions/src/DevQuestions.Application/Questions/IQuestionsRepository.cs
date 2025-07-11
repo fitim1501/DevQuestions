36e91a055344650d08da6131f8f990b21324ee55
michil <fitim1501@gmail.com>
2025-07-11 09:34:47 +0900

    asdfadsf

--- DevQuestions/src/DevQuestions.Application/Questions/IQuestionsRepository.cs
+++ DevQuestions/src/DevQuestions.Application/Questions/IQuestionsRepository.cs
@@ -7,6 +7,7 @@ public interface IQuestionsRepository
     Task<Guid> AddAsync(Question question, CancellationToken cancellationToken);
 
     Task<Guid> SaveAsync(Question question, CancellationToken cancellationToken);
+    
 
     Task<Guid> DeleteAsync(Guid questionId, CancellationToken cancellationToken);
 
