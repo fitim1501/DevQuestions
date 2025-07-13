ccf87cb1e3c96c4c729b3f135363a4df253591fa
michil <fitim1501@gmail.com>
2025-07-13 15:02:38 +0900

    title

---
+++ DevQuestions/src/DevQuestions.Application/Questions/IQuestionService.cs
@@ -0,0 +1,8 @@
+﻿using DevQuestions.Contracts.Questions;
+
+namespace DevQuestions.Application.Questions;
+
+public interface IQuestionService
+{
+    Task Create(CreateQuestionDto questionDto, CancellationToken cancellationToken);
+}
