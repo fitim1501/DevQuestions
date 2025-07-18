e1a424568fe9ca9fa4461ae82d518ffaf1ca81d0
michil <fitim1501@gmail.com>
2025-07-19 00:05:19 +0900

    test

---
+++ DevQuestions/src/DevQuestions.Infrastructure.Postgres/QuestionsDbContext.cs
@@ -0,0 +1,9 @@
+﻿using DevQuestions.Domain.Questions;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevQuestion.Infrastructure.Postgres;
+
+public class QuestionsDbContext : DbContext
+{
+    public DbSet<Question> Questions { get; set; }
+}
