803624e8058351c02f254fe9e65c4f9e37421503
michil <fitim1501@gmail.com>
2025-07-22 20:23:22 +0900

    sdfsafdasf

--- DevQuestions/src/DevQuestions.Application/Questions/CreateQuestionValidator.cs
+++ DevQuestions/src/DevQuestions.Application/Questions/CreateQuestionValidator.cs
@@ -7,10 +7,15 @@ public class CreateQuestionValidator : AbstractValidator<CreateQuestionDto>
 {
     public CreateQuestionValidator()
     {
-        RuleFor(x => x.Title).NotEmpty().MaximumLength(500).WithMessage("Заголовок невалидный");
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Заголовок не может быть пустым")
+            .MaximumLength(500).WithMessage("Заголовок невалидный");
 
-        RuleFor(x => x.Text).NotEmpty().MaximumLength(5000).WithMessage("Текст невалидный");
+        RuleFor(x => x.Text)
+            .NotEmpty().WithMessage("Заголовок не может быть пустым")
+            .MaximumLength(5000).WithMessage("Текст невалидный");
 
-        RuleFor(x => x.UserId).NotEmpty();
+        RuleFor(x => x.UserId)
+            .NotEmpty();
     }
 }
--- DevQuestions/src/Shared/Error.cs
+++ DevQuestions/src/Shared/Error.cs
@@ -1,13 +1,16 @@
-﻿namespace Shared;
+﻿using System.Text.Json.Serialization;
+
+namespace Shared;
 
 public record Error
 {
     public string Code { get; }
     public string Message { get; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public ErrorType Type { get; }
     public string? InvalidField { get; }
 
-    private Error(string code, string message, ErrorType type, string? invalidField = null)
+    public Error(string code, string message, ErrorType type, string? invalidField = null)
     {
         Code = code;
         Message = message;
@@ -30,8 +33,23 @@ public record Error
 
 public enum ErrorType
 {
+    /// <summary>
+    /// Ошибка с валидацией.
+    /// </summary>
     VALIDATION,
+
+    /// <summary>
+    /// Ошибка ничего не найдено.
+    /// </summary>
     NOT_FOUND,
+
+    /// <summary>
+    /// Ошибка сервера.
+    /// </summary>
     FAILURE,
+
+    /// <summary>
+    /// Ошибка конфликт.
+    /// </summary>
     CONFLICT,
 }
