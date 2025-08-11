04c8b9949b1a9fdfa69db079e3084f12f3d777d1
michil <fitim1501@gmail.com>
2025-08-11 21:37:50 +0900

    sdfafd sdfafdasfd

---
+++ DevQuestions/src/DevQuestions.Infrastructure.Communication/UserCommunicationService.cs
@@ -0,0 +1,11 @@
+﻿using CSharpFunctionalExtensions;
+using DevQuestions.Application.Communication;
+using Shared;
+
+namespace DevQuestions.Infrastructure.Communication;
+
+public class UsersCommunicationService: IUsersCommunicationService
+{
+    public Task<Result<long, Failure>> GetUserRating(Guid userId, CancellationToken cancellationToken = default) 
+        => throw new NotImplementedException();
+}
