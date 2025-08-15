cf76b75cc0d8e50fa3c326c8b3150bb96c211ddf
michil <fitim1501@gmail.com>
2025-08-15 18:11:53 +0900

    test

---
+++ DevQuestions/src/Core/Infrastructure/Infrastructure.S3/S3Provider.cs
@@ -0,0 +1,11 @@
+﻿using DevQuestions.Application.FilesStorage;
+
+namespace Infrastructure.S3;
+
+public class S3Provider : IFilesProvider
+{
+    public Task<string> UploadAsync(Stream stream, string key, string bucket) => throw new NotImplementedException();
+    public Task<string> GetUrlByIdAsync(Guid fileId, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public Task<Dictionary<Guid, string>> GetUrlsByIdAsync(IEnumerable<Guid> fileIds, CancellationToken cancellationToken) => throw new NotImplementedException();
+}
---
+++ DevQuestions/src/Web/Web/Program.cs
@@ -0,0 +1,22 @@
+using DevQuestions.Web;
+using DevQuestions.Web.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddProgramDependencies();
+
+var app = builder.Build();
+
+app.UseExceptionMiddleware();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(option => option.SwaggerEndpoint("/openapi/v1.json", "DevQuestions"));
+}
+
+app.MapControllers();
+
+app.MapGet();
+
+app.Run();
